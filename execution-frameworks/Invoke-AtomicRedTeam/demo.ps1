9a4ea9852d962b6a06227fadf492c4bb7f35b1fb
Denys Iuzvyk <den@simplerity.com>
2019-09-02 11:38:29 +0300

    WIP

--- execution-frameworks/Invoke-AtomicRedTeam/demo.ps1
+++ execution-frameworks/Invoke-AtomicRedTeam/demo.ps1
@@ -17,7 +17,7 @@ $techniques = @("T1055")#, "T1057", "T1088","T1050", "T1053", "T1059", "T1060")
 Import-Module C:\atomic-red-team\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam.psm1  -Force
 Foreach ($t in $techniques) {
     Write-Host "Executing $t technique" -fore green
-    Invoke-AtomicTest $t -Uuid $t"_atomicteest" -PathToAtomicsFolder C:\atomic-red-team\atomics -Verbose
+    Invoke-AtomicTest $t -Uuid $t"_atomictest" -PathToAtomicsFolder C:\atomic-red-team\atomics -Verbose
     #Write-Host "C:\atomic-red-team\atomics\$t\$t.yaml"
     #$tObj= Get-AtomicTechnique -Path "C:\atomic-red-team\atomics\$t\$t.yaml"
     #Invoke-AtomicTest $tObj -Verbose
